82770a2cd9ac3895a266d8b36048226b1ced9fde
George Baroi <baroi.george@gmail.com>
2022-07-26 11:42:15 -0400

    Done with Assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // write your code here
+//grab form using query selector
+let form= document.querySelector('#new-ramen')
+//add submit event listener to form
+form.addEventListener('submit', (e) =>{e.preventDefault()
+    let newNameVal = e.target['new-name'].value
+    let p = document.createElement('p')
+    p.textContent = newNameVal
+
+
+
+
+})
+
+
+
+
+let newRestaurant = document.querySelector('#new-restaurant')
+
+
+
+
+
+
+
